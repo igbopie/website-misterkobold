252520f23aee62d6c13dd1f5b8e667abbed143c7
Ignacio Bona <igbopie@gmail.com>
2016-06-20 21:53:53 -0700

    remove data

--- src/server/utils/email.js
+++ src/server/utils/email.js
@@ -4,8 +4,8 @@ var mg = require('nodemailer-mailgun-transport');
 // This is your API key that you retrieve from www.mailgun.com/cp (free up to 10K monthly emails)
 var auth = {
   auth: {
-    api_key: 'key-275a9e1af0dcb1697b6687f14a9eb4d2',
-    domain: 'sandbox38d5e69b89534c07a52f1af49d882b2a.mailgun.org'
+    api_key: process.env.NODEMAILER_API_KEY,
+    domain: process.env.NODEMAILER_DOMAIN
   }
 }
 
@@ -14,16 +14,16 @@ var transporter = nodemailer.createTransport(mg(auth));
 
 exports.sendContact = function (email, name, phone, comments, callback) {
   transporter.sendMail({
-    from: 'misterkobold@gmail.com',
-    to: 'misterkobold@gmail.com',
+    from: process.env.NODEMAILER_EMAIL,
+    to: process.env.NODEMAILER_EMAIL,
     subject: 'Contacto',
     text: "Nombre: "+name+"\nTelefono: "+phone+"\nEmail: "+email+"\nComentarios: "+comments
   }, callback);
 }
 exports.sendReclamations = function (email, name, phone, comments, callback) {
   transporter.sendMail({
-    from: 'misterkobold@gmail.com',
-    to: 'misterkobold@gmail.com',
+    from: process.env.NODEMAILER_EMAIL,
+    to: process.env.NODEMAILER_EMAIL,
     subject: 'Reclamacion',
     text: "Nombre: "+name+"\nTelefono: "+phone+"\nEmail: "+email+"\nMotivo de la reclamacion: "+comments
   }, callback);
